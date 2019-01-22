42dfffbb43dfa8e4476989243883a9c0530ca2b9
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-22 14:44:05 +0200

    Add Event and Client creation

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
 
+var idGenerator = {
+    lastId : 0,
+    next : function() { return ++this.lastId; }
+};
+
+
+main();
+
 function main() {
    
+    var event = new Event("New year party", true);
+    var event2 = new Event("Wedding", false);
+
+    console.log(event.id);
+    console.log(event2.id);
+
+    var client = new Client("Ivan", "Georgiev", "male", 25);
+    console.log(client.firstName);
+    console.log(client.getFullName());
+}
+
+function Event(name, adultOnly) {
+    this.id = idGenerator.next();
+    this.name = name;
+    this.adultOnly = adultOnly;
+}
+
+function Client(firstName, lastName, gender, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.getFullName = function() { return this.firstName + " " + this.lastName; };
+    this.gender = gender;
+    this.age = age;
 }
