f94a0ea4bff69e0f852a6569e6927df0b3cf6668
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-23 18:59:20 +0200

    Move options html to index

--- views.js
+++ views.js
@@ -1,27 +1,14 @@
 
-var PAGE_ID = "page";
+var TABLE_ID = "table_events";
 
 function displayEvents() {
-    clearElement(PAGE_ID);
 
-    var page = document.getElementById(PAGE_ID);
-
-    var optionsGroup = document.createElement("div");
-    optionsGroup.className = "options";
-    var addUI = getCreationUI();
-    var modUI = getModifyUI();
-    var removalUI = getRemovalUI();
-    var evTable = createEventTable();
-
-    optionsGroup.appendChild(addUI);
-    optionsGroup.appendChild(modUI);
-    optionsGroup.appendChild(removalUI);
-    page.appendChild(optionsGroup);
-    page.appendChild(evTable);
+    var evTable = document.getElementById(TABLE_ID);
+    evTable.innerHTML = "";
+    fillEventTable(evTable);
 }
 
-function createEventTable() {
-    var table = document.createElement("table");
+function fillEventTable(table) {
 
     for(var key in events) {
         var event = events[key];
@@ -38,66 +25,12 @@ function createEventTable() {
 
         table.appendChild(row);
     }
-    return table;
-}
-
-function clearElement(id) {
-    var element = document.getElementById(id);
-    element.innerHTML = "";
 }
 
 function createElementWithText(elType, text) {
     var element = document.createElement(elType);
     element.innerHTML = text;
     return element;
-}
-
-function getRemovalUI() {
-    var div = document.createElement("div");
-
-    var form = "\
-    <form action='JavaScript:removeEvent()' onsubmit='return validateRemoval()'>\
-    <h4>Remove Event:</h4>\
-    <label for='input_remove_id'>ID</label>     <input type='text' name='id' id='input_remove_id'/>\
-    <input type='submit' value='Remove' class='submit_btn'/>\
-    </form>";
-    
-    div.innerHTML = form;
-
-    return div;
-}
-
-function getModifyUI() {
-    var div = document.createElement("div");
-
-    var form = "\
-    <form action='JavaScript:modifyEvent()' onsubmit='return validateModify()'>\
-    <h4> Modify Event:</h4>\
-    <label for='input_mod_id'>ID</label>                <input type='text' name='id' id='input_mod_id'/>\
-    <label for='input_mod_name'>Name</label>            <input type='text' name='name' id='input_mod_name'/>\
-    <label for='input_mod_adult'>Adult Only</label>     <input type='checkbox' name='adultOnly' id='input_mod_adult'/>\
-    <input type='submit' value='Modify' class='submit_btn'/>\
-    </form>";
-    
-    div.innerHTML = form;
-
-    return div;
-}
-
-function getCreationUI() {
-    var div = document.createElement("div");
-
-    var form = "\
-    <form action='JavaScript:createEvent()' onsubmit='return validateCreation()'>\
-    <h4> Add New Event:</h4>\
-    <label for='input_add_name'>Name</label>            <input type='text' name='name' id='input_add_name'/>\
-    <label for='input_add_adult'>Adult Only</label>     <input type='checkbox' name='adultOnly' id='input_add_adult'/>\
-    <input type='submit' value='Add' class='submit_btn'/>\
-    </form>";
-    
-    div.innerHTML = form;
-
-    return div;
 }
 
 function validateCreation() {
